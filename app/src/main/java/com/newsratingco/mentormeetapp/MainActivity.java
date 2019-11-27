c8d3e2acfa61f527bf3ae319583859a45774b4a4
sauravchy1 <55821228+sauravchy1@users.noreply.github.com>
2019-11-26 18:40:14 -0600

    Initial Commit

---
+++ app/src/main/java/com/newsratingco/mentormeetapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.newsratingco.mentormeetapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
